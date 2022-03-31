5d9f7b016184f43d24cde68e4fad48db885054b1
Joey Kamsteeg <joey_kamsteeg@live.nl>
2022-03-31 18:44:11 +0200

    setup vue2 project with vite

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Button label="Click Me" @onClick="handleClick" />
+  </div>
+</template>
+
+<script lang="ts">  
+  import Vue from 'vue';
+  import Button from './components/Buttons/Button.vue';
+
+  export default Vue.extend({
+    name: "App",
+    components: { Button },
+    methods: {
+      handleClick() {
+        console.log('Clicked from Vite Vue2 app')
+      }
+    }
+  })
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Buttons/Button.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="button" @click="$emit('onClick')">
+        {{ label }}
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend ( {
+        name: 'Button',
+        props: {
+            label: {
+                type: String,
+                required: true
+            }
+        }
+    } );
+</script>
+
+<style lang="css" scoped>
+    .button {
+        font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+        background-color: cornflowerblue;
+        padding: 0.75rem 2rem;
+        color: #FFF;
+        text-align: center;
+        cursor: pointer;
+        border-radius: 25px;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        -webkit-user-select: none;
+        user-select: none;
+        font-weight: bold;
+    }
+</style>
