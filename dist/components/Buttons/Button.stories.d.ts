a92be8ed330dd3107bc5c8cdcb689ceb6a1f1ccd
Joey Kamsteeg <joey.k@socialdeal.nl>
2022-04-01 14:23:54 +0200

    Typescript declaration files

---
+++ dist/components/Buttons/Button.stories.d.ts
@@ -0,0 +1,12 @@
+import Button from './Button.vue';
+declare const _default: {
+    title: string;
+    component: import("vue").VueConstructor<Button>;
+    argTypes: {
+        label: {
+            type: string;
+        };
+    };
+};
+export default _default;
+export declare const Default: any;
